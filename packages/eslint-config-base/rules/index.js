31dfdd1126bfadb23411f381ad79aa21a2e13f33
Yorick van Klinken <yorick@insiders.nl>
2021-06-03 14:13:25 +0000

    CIT-4800 turning no-continue off

--- packages/eslint-config-base/rules/index.js
+++ packages/eslint-config-base/rules/index.js
@@ -14,6 +14,7 @@ module.exports = {
                 "selector": "WithStatement",
                 "message": "`with` is disallowed in strict mode because it makes code impossible to predict and optimize."
             }
-        ]
+        ],
+        "no-continue": "off"
     }
 };
